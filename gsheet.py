08e2170b8185b16c000204d299eada45d0abfcca
Jackowol <36078274+Jackowol@users.noreply.github.com>
2020-02-12 19:53:21 -0500

    html v3, first python scripts

--- gsheet.py
+++ gsheet.py
@@ -1,23 +1,22 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import google.auth
+from google.oauth2 import service_account
 
+credentials = service_account.Credentials.from_service_account_file(
+    '/path/to/key.json')
 
+scoped_credentials = credentials.with_scopes(
+    ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive'])
 
-# API Credentials
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('projet-signatures-90b29f9573cc.json', scope)
-client = gspread.authorize(creds)
+client = gspread.authorize(scoped_credentials)
+
+# Remplacer l'ID par le vrai
 sheet = client.open_by_key('1GNSsDSjD7tVinR8fQXLI3PxWcxk90dNWYSskO7vzKB8').sheet1
 
 DEBUG = False
 
-#asasd
-
-
+# For testing purposes only
 def findUser(email):
-    """
-    For testing purposes only.
-    """
     print('BEGIN findUser()')
     cell = sheet.find(email)
     print(email + " repéré à " + "row : " + str(cell.row) + " col : " + str(cell.col))
@@ -38,15 +37,10 @@ def getInfo(email):
         cell = sheet.find(email)
         
     values = sheet.row_values(cell.row)
-    
-    # adresse du bureau 1
-    values[9] = values[9].split('\n')
-    
-    if(values[10]):
-        values[10] = values[10].split('\n')
 
     if(DEBUG):
         print(values)
         print(len(values))
+
     return values
    
